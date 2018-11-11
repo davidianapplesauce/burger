25b668835c1e96368fd4ef13b5784bcd9f73b09d
David Applesauce <dsweck@gmail.com>
2018-11-11 17:51:04 -0500

    burger app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,36 +2,43 @@ var express = require("express");
 
 var router = express.Router();
 
-//Imports the burger.js model
+//Import the model (burger.js)
 var burger = require("../models/burger.js");
 
 //Routes
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
-  });
-
-  //post new burger
-router.post("/", function(req, res){
-	console.log(req.body.input);
-	burger.create(req.body.input, function(){
-		res.redirect("/");
-	})
 });
-//update devoured burgers
-router.put("/:id", function(req, res) {
-    var id = req.params.id;
-  
-    console.log(id);
-  
-    burger.update("devoured",true, id, function(){
+
+//all burgers
+router.get("/api", function (req, res) {
+    burger.all(function (data) {
+        res.json(data);
+    })
+});
+
+//posts new burger
+router.post("/", function (req, res) {
+    console.log(req.body.input);
+    burger.create(req.body.input, function () {
         res.redirect("/");
     })
-  });
-  
-  module.exports = router;
+});
+//update devoured burgers
+router.put("/:id", function (req, res) {
+    var id = req.params.id;
+
+    console.log(id);
+
+    burger.update("devoured", true, id, function () {
+        res.redirect("/");
+    })
+});
+
+module.exports = router;
